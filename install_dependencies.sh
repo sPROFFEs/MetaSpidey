24de7c17650136ab84afdb966b92e17651000221
pr0ff3 <150958256+sPROFFEs@users.noreply.github.com>
2024-03-19 00:29:24 +0100

    Add files via upload

---
+++ install_dependencies.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Instalar dependencias utilizando pip
+pip install argparse requests beautifulsoup4
+
+# Instalar paquete para manipulación de URLs
+pip install urllib3
+
+# Instalar paquete para análisis de URL
+pip install urllib3
+
+# Instalar paquete para manipulación de archivos y procesos
+pip install subprocess
